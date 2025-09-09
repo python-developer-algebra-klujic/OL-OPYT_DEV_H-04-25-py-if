2be742439a79eabaf7f6f5a081b50b5487ad08b7
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-09 20:59:52 +0200

    Create py_if_04.py

---
+++ py_if_04.py
@@ -0,0 +1,18 @@
+'''
+Napisite program koji ispisuje bozicno drvce, a korisnik moze birati znak i visinu
+
+Primjer:
+Visina je 8
+Znak je *
+
+       *
+      ***
+     *****
+    *******
+   *********
+  ***********
+ *************
+***************
+     | | |
+     | | |
+'''
