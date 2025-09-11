d8b51de5a900e7a7d52e20a18281d75fbb0767cb
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-11 18:34:46 +0200

    Update py_if_04.py

--- py_if_04.py
+++ py_if_04.py
@@ -16,3 +16,22 @@ Znak je *
      | | |
      | | |
 '''
+
+
+height = int(input('Upisite visinu jelke: '))
+symbol = input('Upisite simbol za crtanje jelke: ')
+
+
+# jelka
+for i in range(height + 1):
+    # ispis praznina -> ' '
+    print(' ' * (height - i), end='')
+    
+    # ispis zvjezdica -> '*'
+    print(symbol * ((i * 2) - 1))
+
+
+# stablo
+for j in range(3):
+    print(' ' * (height - 2), end='')
+    print('|' * 3)
